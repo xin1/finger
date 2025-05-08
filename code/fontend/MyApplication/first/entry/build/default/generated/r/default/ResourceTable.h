05db392a6fbf5877a42d5b8f95890219d71f2513
xin1 <81465751+xin1@users.noreply.github.com>
2025-05-08 22:31:25 +0800

    Add files via upload

---
+++ code/fontend/MyApplication/first/entry/build/default/generated/r/default/ResourceTable.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef RESOURCE_TABLE_H
+#define RESOURCE_TABLE_H
+
+#include<stdint.h>
+
+namespace OHOS {
+const int32_t STRING_ENTRYABILITY_DESC = 0x01000002;
+const int32_t STRING_ENTRYABILITY_LABEL = 0x01000003;
+const int32_t STRING_APP_NAME = 0x01000000;
+const int32_t STRING_MODULE_DESC = 0x01000004;
+const int32_t COLOR_START_WINDOW_BACKGROUND = 0x01000005;
+const int32_t MEDIA_11111 = 0x01000006;
+const int32_t MEDIA_3 = 0x01000007;
+const int32_t MEDIA_A = 0x01000008;
+const int32_t MEDIA_APP_ICON = 0x01000001;
+const int32_t MEDIA_BACK10 = 0x01000009;
+const int32_t MEDIA_BACK11 = 0x0100000a;
+const int32_t MEDIA_BACK12 = 0x0100000b;
+const int32_t MEDIA_BACK13 = 0x0100000c;
+const int32_t MEDIA_BACK14 = 0x0100000d;
+const int32_t MEDIA_BACK15 = 0x0100000e;
+const int32_t MEDIA_BACK16 = 0x0100000f;
+const int32_t MEDIA_BACK2 = 0x01000010;
+const int32_t MEDIA_BACK3 = 0x01000011;
+const int32_t MEDIA_BACK4 = 0x01000012;
+const int32_t MEDIA_BACK5 = 0x01000013;
+const int32_t MEDIA_BACK6 = 0x01000014;
+const int32_t MEDIA_BACK9 = 0x01000015;
+const int32_t MEDIA_BAOGAO1 = 0x01000016;
+const int32_t MEDIA_DATA1 = 0x01000017;
+const int32_t MEDIA_DATA3 = 0x01000018;
+const int32_t MEDIA_HAND = 0x01000019;
+const int32_t MEDIA_ICON = 0x0100001a;
+const int32_t MEDIA_IMG = 0x0100001b;
+const int32_t MEDIA_IMG1 = 0x0100001c;
+const int32_t MEDIA_JIDIAN = 0x0100001d;
+const int32_t MEDIA_LOGO = 0x0100001e;
+const int32_t MEDIA_SHOUSHI3 = 0x0100001f;
+const int32_t MEDIA_SHOUSHI4 = 0x01000020;
+const int32_t MEDIA_SHOUSHI5 = 0x01000021;
+const int32_t MEDIA_SHOUSHI6 = 0x01000022;
+const int32_t MEDIA_SHOUSHI7 = 0x01000023;
+const int32_t MEDIA_SHOUSHI8 = 0x01000024;
+const int32_t MEDIA_TOUXIANG2 = 0x01000025;
+const int32_t MEDIA_TOUXIANG3 = 0x01000026;
+const int32_t MEDIA_TOUXIANG4 = 0x01000027;
+const int32_t MEDIA_TOUXIANGBACK5 = 0x01000028;
+const int32_t MEDIA_YISHENG1 = 0x01000029;
+const int32_t MEDIA_YISHENG2 = 0x0100002a;
+const int32_t MEDIA_YISHENG3 = 0x0100002b;
+const int32_t MEDIA_YISHENG4 = 0x0100002c;
+const int32_t PROFILE_MAIN_PAGES = 0x0100002d;
+}
+#endif
